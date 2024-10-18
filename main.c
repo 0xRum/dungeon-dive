0eba11f8aa4cc62cab8e4c4adbbd87e2480555ce
0xR <114037840+0xRum@users.noreply.github.com>
2024-10-18 09:31:33 -0400

    Create main.c
    outlined main

---
+++ main.c
@@ -0,0 +1,6 @@
+//Where the program begins. takes roomList as a command line argument. holds the following functions
+//createDungeon: takes the pointer to the room array and picks random room in dungeon (create copy of the room then add to the linked list)Make dungeon a 2d linked list that is bi-directional
+//printDungeon:takes pointer to start of dungeon, loops through the linked list, prints off room code name in small format
+//deleteDungeon:takes pointer to start of dungeon and frees memory allocated for entire dungeon
+//main: creates room array from the file, asks user for size of dungeon to be created, creates dungeon that size, prints dungeon, deletes dungeon.
+
