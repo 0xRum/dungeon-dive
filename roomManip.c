aee2b4a28407cd73d57a5dcdd614b9e80f03b469
0xR <114037840+0xRum@users.noreply.github.com>
2024-10-18 09:31:42 -0400

    Create roomManip.c
    outlined roomManip

---
+++ roomManip.c
@@ -0,0 +1,7 @@
+//contains any function related to "Rooms"
+//such as
+//room structure definition w/ following properties
+//Room name|Room code|Room Description|4 pointers to other rooms.
+
+//holds roomCreate
+//holds readRoomFile
