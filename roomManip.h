b6a2dc4a8ae28496a000f87f1e9abd06ecfd98f7
0xR <114037840+0xRum@users.noreply.github.com>
2024-10-19 16:53:15 -0400

    added forward declaration for room
    fwd declaration

--- roomManip.h
+++ roomManip.h
@@ -1,4 +1,13 @@
 #ifndef ROOMMANIP_H
 #define ROOMMANIP_H
 
+typedef struct ROOM ROOM;
+
+struct ROOM{
+    char code[10];
+    char name[256];
+    char description[1024];
+    ROOM *north, *east, *south, *west;
+};
+
 #endif
