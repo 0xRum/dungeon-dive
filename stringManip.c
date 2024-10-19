c69be785c52cdfba0264dff8d3f2bc060b3f3dcf
0xR <114037840+0xRum@users.noreply.github.com>
2024-10-19 16:53:34 -0400

    added str_trim

--- stringManip.c
+++ stringManip.c
@@ -1,3 +1,21 @@
 //contains any functions related to string manipulation
 //such as 
-//str_trim|str_cut
+//str_trim: removes the whitespace from each end of  a string sent to it, returning point to new string
+//str_cut: takes three args, the string pointer, an int representing the start, and an int representing the end. Returns a point to the substring that begins at the start and ends at the end
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "stringManip.h"
+
+char *str_trim(char *str){
+    char *end;
+    //trim leading
+    while(isspace(*str)) str++;
+     //trim trailing 
+    end = str + strlen(str) - 1;
+    while(end > str && isspace(*end)) end--;
+    //null terminate
+    *(end + 1) = 0;
+    return str;
+} 
