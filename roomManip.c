d0364393a4abcf8d112b4c51385d103723981835
0xR <114037840+0xRum@users.noreply.github.com>
2024-10-21 10:18:34 -0400

    Update roomManip.c
    added roomcreate and readroomfile

--- roomManip.c
+++ roomManip.c
@@ -5,3 +5,20 @@
 
 //holds roomCreate
 //holds readRoomFile
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "roomManip.h"
+#include "stringManip.h"
+
+ROOM *roomCreate(ROOM *room){
+    if (room == NULL) return NULL;
+
+    ROOM *copy = malloc(sizeof(ROOM));
+    if (!copy) return NULL;
+}
+
+ROOM *readroomFile(const char *filename, int *roomCount){
+    FILE *fp = fopen(filename, "r");
+    if (!fp) return NULL;
+}
