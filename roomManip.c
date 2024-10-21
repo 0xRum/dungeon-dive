77cbcfb360cee29d89060aa83c2751b57f7accdf
0xR <114037840+0xRum@users.noreply.github.com>
2024-10-21 13:07:07 -0400

    Update roomManip.c
    reads room data from a text file and stores it, handles reallocation, error handling

--- roomManip.c
+++ roomManip.c
@@ -12,11 +12,10 @@
 #include "stringManip.h"
 
 ROOM *roomCreate(ROOM *room){
-    if (room == NULL) return NULL;
-
-    ROOM *copy = malloc(sizeof(ROOM));
+    ROOM *copy = malloc(sizeof(ROOM)); //allocate memory for enw room
     if (!copy) return NULL;
-    *copy = *room;
+    *copy = *room; //copy room contents
+    return copy; //return new room copy
 }
 
 ROOM *readroomFile(const char *filename, int *roomCount){
@@ -25,7 +24,33 @@ ROOM *readroomFile(const char *filename, int *roomCount){
         printf("Error opening file %s\n", filename);
         return NULL;
     }
-    int roomba = 2;
-    int nextRooms = 0;
+    
+    int roomba = 2; //starts with 2 rooms
+    int nextRooms = 0; //next rooms begins at 0
     ROOM *rooms = malloc(sizeof(ROOM) * roomba);
+    char line[2048];
+
+    while (fgets(line, sizeof(line), fp)) {
+        if (strncmp(line, "Room Name:", 10) == 0) {
+            if (roomba >= nextRooms) {
+                nextRooms *= 2;  // double the room
+                ROOM *temp = realloc(rooms, sizeof(ROOM) * nextRooms);
+                if (!temp) {
+                    free(rooms);  // free original memory
+                    return NULL;
+                }
+                rooms = temp;
+
+            }
+            strcpy(rooms[roomba].name, str_trim(line + 10));
+        } else if (strncmp(line, "Room Code:", 10) == 0) {
+            strcpy(rooms[roomba].code, str_trim(line + 10));
+        } else if (strncmp(line, "Room Description:", 17) == 0) {
+            strcpy(rooms[roomba].description, str_trim(line + 17));
+            roomba++; // move to the next room after completing one entry
+        }
+    }
+    fclose(fp);
+    *roomCount = nextRooms;
+    return rooms;
 }
