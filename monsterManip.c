117a14763ab0d01d6967f3166bf45c30f5a83c72
0xR <114037840+0xRum@users.noreply.github.com>
2024-12-19 10:33:46 -0500

    Update monsterManip.c
    switched strcat lines

--- monsterManip.c
+++ monsterManip.c
@@ -4,18 +4,21 @@
 #include <ctype.h>
 #include "monsterManip.h"
 
-char *str_troll(char *str) {
+char *str_troll(char *str)
+{
     char *end;
 
     // Trim leading whitespace
-    while (isspace((unsigned char)*str)) str++;
+    while (isspace((unsigned char)*str))
+        str++;
 
     if (*str == '\0') // All spaces
         return str;
 
     // Trim trailing whitespace
     end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end)) end--;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
 
     // Write new null terminator
     *(end + 1) = '\0';
@@ -23,18 +26,21 @@ char *str_troll(char *str) {
     return str;
 }
 
- MONSTER *monsterRoomFile(const char *filename, int *monsterCount){
+MONSTER *monsterRoomFile(const char *filename, int *monsterCount)
+{
     FILE *fp = fopen(filename, "r");
-    if (!fp) {
+    if (!fp)
+    {
         printf("Error opening file %s\n", filename);
         return NULL;
     }
-    
-    int monsterCapacity = 2; 
-    int monsterIndex = 0; 
+
+    int monsterCapacity = 2;
+    int monsterIndex = 0;
     MONSTER *monsters = malloc(sizeof(MONSTER) * monsterCapacity);
 
-    if(!monsters){
+    if (!monsters)
+    {
         printf("Error allocating memory for monsters\n");
         fclose(fp);
         return NULL;
@@ -42,12 +48,16 @@ char *str_troll(char *str) {
 
     char line[50];
 
-    while(fgets(line, sizeof(line), fp)) {
-        if ((line[0] == '*')){
-            if(monsterIndex >= monsterCapacity) {
+    while (fgets(line, sizeof(line), fp))
+    {
+        if ((line[0] == '*'))
+        {
+            if (monsterIndex >= monsterCapacity)
+            {
                 monsterCapacity *= 2;
                 MONSTER *temp = realloc(monsters, sizeof(MONSTER) * monsterCapacity);
-                if(!temp){
+                if (!temp)
+                {
                     printf("Error reallocating memory for monsters\n");
                     free(monsters);
                     fclose(fp);
@@ -56,21 +66,20 @@ char *str_troll(char *str) {
                 monsters = temp;
             }
 
-            //trim line for monster
+            // trim line for monster
             strcpy(monsters[monsterIndex].name, line);
 
-            while(fgets(line, sizeof(line), fp) && strcmp(str_troll(line), "") !=0){
-                strcat(monsters[monsterIndex].name, "\n");
+            while (fgets(line, sizeof(line), fp) && strcmp(str_troll(line), "") != 0)
+            {
                 strcat(monsters[monsterIndex].name, line);
+                strcat(monsters[monsterIndex].name, "\n");
             }
 
             monsterIndex++;
-            
         }
-
     }
-    
+
     fclose(fp);
     *monsterCount = monsterIndex;
     return monsters;
- }
+}
