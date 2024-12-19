ed94bdad2e55f8ecfe1df66c02d87a7bc700fa57
0xR <114037840+0xRum@users.noreply.github.com>
2024-12-19 10:35:03 -0500

    Update monsterManip.h
    add roomcode

--- monsterManip.h
+++ monsterManip.h
@@ -3,10 +3,11 @@
 
 typedef struct MONSTER MONSTER;
 
-struct MONSTER {
+struct MONSTER
+{
     char name[2048];
+    char roomCode[10];
 };
-
 
 char *str_troll(char *str);
 MONSTER *monsterRoomFile(const char *fileName, int *monsterCount);
